f781db89d7efa18bf91cfaa0c293345291d42b98
macimattia01 <maci.mattia.2001@gmail.com>
2022-12-14 12:09:40 +0100

    new user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const Group = require("../models/group");
 const newUser = (req,res,next) => {
     let emailisin = User.exists({email: req.body.email},(err,data)=>{
         if(err || !data) return res.status(404).json({message:"Group does not exist",status:404});
@@ -6,8 +7,26 @@ const newUser = (req,res,next) => {
         if(err || !data) return res.status(404).json({message:"Group does not exist",status:404});
     });
     if(emailisin){
-
+        return res.status(400).json({message:'email already registered',status:400});
     }
+    if(usernameisin){
+        return res.status(400).json({message:'username already taken',status:400});
+    }
+    const newUser = (req,res)=>{
+        const newUser=new User({
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password,
+            image:req.body.image,
+            description:req.body.description,
+            reputation:req.body.reputation,
+            isVerified:req.body.isVerified
+        })
+        newUser.save((err,data)=>{
+            if(err) return res.status(500).json({Error:err,status:500});
+            return res.status(201).json({data:data,status:201});
+        })
+    };
     res.json({message: "prova POST"});
 }
 module.exports = {newUser};
