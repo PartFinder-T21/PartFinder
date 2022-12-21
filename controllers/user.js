8a1cb08c322be88068d9a72e207b320a0493c247
GvShepardo <oloccin02@gmail.com>
2022-12-21 17:29:12 +0100

    fixed login method in user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,60 +1,51 @@
-const Group = require("../models/group");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const User = require("../models/user")
-const newUser = (req,res) => {
-        const newUser=new User({
-            username:req.body.username,
-            email:req.body.email,
-            password:req.body.password,
-            image:req.body.image,
-            description:req.body.description,
-            reputation:0,
-            isVerified:false,
-            groups:[]
-        })
-        newUser.save((err,data)=>{
-            if(err){
-                if(err.code!=11000)return res.status(500).json({Error:err,status:500});
-                else return res.status(400).json({message:"Username and/or email is already registered",status:400});
-            }
-            return res.status(201).json({data:data,status:201});
-        })
+const dotenv = require('dotenv').config({path: '/home/sheppi/Scrivania/PartFinder/PartFinder/misc/.env'});
+const newUser = async(req,res) => {
+    let hashedPassword = bcrypt.hashSync(req.body.password,bcrypt.genSaltSync(10));
+    const newUser=new User({
+        username:req.body.username,
+        email:req.body.email,
+        password:hashedPassword,
+        image:req.body.image,
+        description:req.body.description,
+        reputation:0,
+        isVerified:false,
+        groups:[]
+    })
+    newUser.save((err,data)=>{
+        if(err){
+            if(err.code!=11000)return res.status(500).json({Error:err,status:500});
+            else return res.status(400).json({message:"Username and/or email is already registered",status:400});
+        }
+        return res.status(201).json({data:data,status:201});
+    })
 }
 
-const login = async (req,res,next) => {
-    let email = req.body.input;
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            );
-    };
-    if(validateEmail){
-        let User = User.findOne({email: req.body.input},(err,data) => {
-            if(err || !data) return res.status(500).json({message:'User or password is wrong',status:500})
-            return JSON.parse(data);
-        });
-    }
-    else{
-        let User = User.findOne({username: req.body.input},(err,data) => {
-            if(err || !data) return res.status(500).json({message:'User or password is wrong',status:500})
-            return JSON.parse(data);
-        });
-    }
-    if(await bcrypt.compare(User.password, req.body.password)){
-        var payload = {username:User.username,id:User._id};
-        var options = {expireIn:21600};
-        var token = jwt.sign(payload,process.env.SUPER_SECRET,options);
+const login = (req,res) => {
+    User.findOne({$or:[{email:req.body.input},{username:req.body.input}]},(err,data)=>{
+        if(err) return res.status(500).json({message:"Unexpected error",status:500});
+        else if(!data) return res.status(400).json({message:"Username or password wrong email check",status:400});
+        else {
+            bcrypt.compare(req.body.password, data.password, (err, result) => {
+                if (err) {
+                    return res.status(500).json({message:"Unexpected error", status: 500});
+                } else if (result) {
+                    let payload = {username: data.username, id: data._id};
+                    let options = {expiresIn: 21600};
+                    let token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
-        res.json({success: true,message: "validation token", token: token,
-            username:User.username,id:User._id,self:"user/tk/"+User._id});
-    }
-    else{
-        return res.status(400).json({message:'User or password is wrong',status:400});
-    }
-
+                    return res.status(200).json({
+                        success: true, message: "validation token", token: token,
+                        username: data.username, id: data._id, self: "user/tk/" + data._id
+                    });
+                } else {
+                    return res.status(400).json({Result:result,message: "Username or password is wrong password check", status: 400});
+                }
+            })
+        }
+    })
 }
 
 module.exports = {newUser,login};
