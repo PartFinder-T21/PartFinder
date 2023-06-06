04e4760db832d1ba74a9690103530dcc7cc45f53
GvShepardo <oloccin02@gmail.com>
2023-06-06 20:03:51 +0200

    fix vitejs.config

--- static/vite.config.js
+++ static/vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
     host: true,
     proxy:{
       "/api": {
-        target:"http://localhost:8080/",
+        target:"https://partfindert21web.onrender.com/",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/,'')
