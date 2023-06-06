7f7e411a64a16abb0da77bfb519365c21dfc8dd4
GvShepardo <oloccin02@gmail.com>
2023-06-06 22:21:20 +0200

    fix cors

--- corsMiddleware.js
+++ corsMiddleware.js
@@ -1,10 +1,9 @@
 
-
-const tokenChecker = function(req, response, next) {
+const corsMiddleware = function(req, response, next) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Credentials", "true");
     response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 };
-module.exports = tokenChecker;
+module.exports = corsMiddleware;
